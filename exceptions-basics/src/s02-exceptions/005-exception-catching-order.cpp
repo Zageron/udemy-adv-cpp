711bf33ec787a87bc80ea407196b50e30482bf77
Adam Bryant <eranegera@gmail.com>
2017-01-28 15:12:14 -0800

    Add lesson 5 progress.

---
+++ exceptions-basics/src/s02-exceptions/005-exception-catching-order.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <exception>
+using namespace std;
+
+void goesWrong()
+{
+	bool error0 = true;
+	bool error1 = false;
+
+	if (error0)
+	{
+		throw bad_alloc();
+	}
+
+	if (error1)
+	{
+		throw exception();
+	}
+}
+
+void exceptionCatchingOrder()
+{
+	try
+	{
+		goesWrong();
+	}
+	catch (bad_alloc &e)
+	{
+		cout << "Caught Exception: " << e.what() << endl;
+	}
+	// Subclasses must come before the parent class in exceptions.
+	catch (exception &e)
+	{
+		cout << "Caught Exception: " << e.what() << endl;
+	}
+}
