39e33d094d93bdbbdb6180a60557e0a77b9e6d85
Adam Bryant <eranegera@gmail.com>
2017-01-28 10:47:10 -0800

    Add lesson 3 progress.

---
+++ exceptions-basics/src/s02-exceptions/003-standard-exceptions.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class CanGoWrong
+{
+public:
+	CanGoWrong()
+	{
+		_ULonglong badSize = 999999999999 * 9999;
+		char *pMemory = new char[badSize];
+		delete[] pMemory;
+	}
+};
+
+
+void standardExceptions()
+{
+	try
+	{
+		CanGoWrong wrong;
+	}
+	catch (bad_alloc &e)
+	{
+		cout << "Caught exception: " << e.what() << endl;
+	}
+
+	cout << "Still running..." << endl;
+}
