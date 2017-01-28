d0c23b484eec92225e17dbeeb159ecffbbfa513b
Adam Bryant <eranegera@gmail.com>
2017-01-28 12:21:34 -0800

    Add lesson 4 progress.

---
+++ exceptions-basics/src/s02-exceptions/004-custom-exceptions.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class MyException : public exception
+{
+public:
+	const char* what() const override
+	{
+		return "Something bad happened!";
+	}
+};
+
+class Test
+{
+public:
+	static void GoesWrong()
+	{
+		throw MyException();
+	}
+};
+
+void customExceptions()
+{
+	try
+	{
+		Test::GoesWrong();
+	}
+	catch (MyException &e)
+	{
+		cout << "Caught Exception: " << e.what() << endl;
+	}
+}
