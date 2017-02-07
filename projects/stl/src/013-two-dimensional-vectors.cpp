dfeaf58d20508676b23571ff3b226e4e1b9d7fe8
Adam Bryant <adam.bryant@skyboxlabs.com>
2017-02-07 09:10:58 -0800

    Add lesson 13 progress.

---
+++ projects/stl/src/013-two-dimensional-vectors.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+void twoDimensionalVectors()
+{
+	std::vector<std::vector<int>> grid(3, std::vector<int>(4, 0));
+
+	for (size_t row = 0; row < grid.size(); row++)
+	{
+		for (size_t col = 0; col < grid[row].size(); col++)
+		{
+			std::cout << grid[row][col] << std::flush;
+		}
+
+		std::cout << std::endl;
+	}
+}
